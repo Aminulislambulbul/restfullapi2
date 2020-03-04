b7f5bf99a935f6007b0e38feb379da50c26b6baf
Aminulislambulbul <m.aminulislambulbul@gmail.com>
2020-03-04 12:08:04 +0600

    one file is edited

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ mongoose.connect(DB, {
 
 // default route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Humayun Product app"});
+    res.json({"message": "Welcome to Aminul Islam Bulbul Product app"});
 });
 
 // listen on port 3000
